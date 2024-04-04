55f9001ea4ca396a9211b6167dc454c63efebb77
shaunaksangdod <shaunaksangdod@gmail.com>
2024-04-04 19:15:14 -0400

    unfucked login

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const PROJECT_NAME = "hazard_mitigation";
 
-let API_HOST = "https://graph2.availabs.org";
-let AUTH_HOST = "https://graph2.availabs.org";
-let CLIENT_HOST = "mitigateny.org";
-let DAMA_HOST = "https://graph2.availabs.org";
-
+let API_HOST = "https://graph.availabs.org";
+let AUTH_HOST = "https://availauth.availabs.org";
+let CLIENT_HOST = "transportny.org";
+let DAMA_HOST = "https://graph.availabs.org";
 
 if (process.env.NODE_ENV === "development") {
-  // API_HOST = "http://localhost:4444";
-  // AUTH_HOST = "http://localhost:4444/auth"
-  // CLIENT_HOST = "localhost:3000";
-  // DAMA_HOST = "https://graph.availabs.org";
+    // API_HOST = "http://localhost:4444";
+    // DAMA_HOST = "http://localhost:4444";
+    // AUTH_HOST = "http://localhost:4444/auth"
+    // CLIENT_HOST = "localhost:3000";
+    // DAMA_HOST = "https://graph.availabs.org";
 }
 
 export { API_HOST, AUTH_HOST, CLIENT_HOST, DAMA_HOST };
