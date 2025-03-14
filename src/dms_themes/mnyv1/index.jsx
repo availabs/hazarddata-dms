064c81cac760660774542daeaaf54ff28af6ab6c
shaunaksangdod <shaunaksangdod@gmail.com>
2025-03-14 15:30:39 -0400

    theme

--- src/dms_themes/mnyv1/index.jsx
+++ src/dms_themes/mnyv1/index.jsx
@@ -18,8 +18,8 @@ const Logo = ({sideNav}) => {
   return (
     <>
       <Link to="/" className={`flex `}>
-        <div className='h-12 px-12 flex items-center text-lg font-medium uppercase'>
-          Hazard Data
+        <div className='h-12 pl-3 pr-2 flex items-center '>
+          <MNYLogo height={50} width={190} color={'#37576b'}/>
         </div>  
       </Link>
     </>
@@ -47,9 +47,9 @@ const theme = {
   },
   heading: {
     "base": "p-2 w-full font-sans font-medium text-md bg-transparent",
-    "1": `text-blue-500 font-bold text-xl tracking-wider py-1 pl-1`,
-    "2": `text-lg tracking-wider`,
-    "3": `text-md tracking-wide`,
+    "1": `font-[500]  text-[#2D3E4C] text-[36px] leading-[36px] tracking-[-.02em] font-[500] underline-offset-8 underline decoration-4 decoration-[#EAAD43] uppercase font-['Oswald'] pb-[12px]`,
+    "2": `font-[500]  text-[#2D3E4C] text-[24px] leading-[24px] scroll-mt-36 font-['Oswald'] pb-[12x]`,
+    "3": `font-[500]  text-[#2D3E4C] text-[16px] leading-[16px] scroll-mt-36 font-['Oswald'] pb-[12x]`,
     "default": ''                                                                        
   },
   levelClasses: {
@@ -59,18 +59,37 @@ const theme = {
     '4': 'pl-6 pt-2 pb-1 text-sm text-slate-400 hover:underline cursor-pointer border-r-2 mr-4',
   },
   layout: {
-    wrapper: 'max-w-[1400px] mx-auto',
-    topnavContainer2:`fixed top-0 z-20 max-w-[1400px] w-full py-2`,
-    sidenavContainer1: 'pr-2',
-    sidenavContainer2: 'sticky top-[119px] h-[calc(100vh_-_125px)] bg-white rounded-lg shadow w-64 pt-2 overflow-y-auto overflow-x-hidden'
+    wrapper: 'max-w-[1440px] mx-auto',
+    wrapper2: 'flex-1 flex items-start flex-col items-stretch max-w-full',
+    wrapper3: 'flex flex-1 px-4 xl:px-[64px]',
+    childWrapper: 'h-full flex-1',
+    topnavContainer2:`fixed top-0 z-20 max-w-[1440px] w-full px-4 pt-[32px] xl:px-[64px] pointer-events-none`,
+    sidenavContainer1: 'pr-2  hidden lg:block min-w-[302px] max-w-[302px] pt-[88px]',
+    sidenavContainer2: 'hidden lg:block sticky top-[120px] h-[calc(100vh_-_125px)] bg-white rounded-lg shadow-md w-full overflow-y-auto overflow-x-hidden'
   },
-  page: { //bg-gradient-to-r from-[#f8f4ec] to-[#fefefe] 
-    container: `bg-[linear-gradient(0deg,rgba(244,244,244,0.96),rgba(244,244,244,0.96)),url('/themes/mny/grid.png')]  bg-[size:500px]`,//`bg-gradient-to-b from-[#F4F4F4] to-[#F4F4F4] bg-[url('/themes/mny/topolines.png')] `,
-    wrapper1: 'w-full h-full flex-1 flex flex-col pt-[93px] py-2', // first div inside Layout
-    wrapper2: 'w-full h-full flex-1 flex flex-row  py-6', // inside page header, wraps sidebar
-    wrapper3: 'flex flex-1 w-full  flex-col border shadow bg-white rounded-lg relative text-md font-light leading-7 p-4 min-h-[calc(100vh_-_102px)]' , // content wrapepr
+  page: {
+    container: `bg-[linear-gradient(0deg,rgba(244,244,244,0.96),rgba(244,244,244,0.96)),url('/themes/mny/topolines.png')]  bg-[size:500px] pb-[4px]`,//`bg-gradient-to-b from-[#F4F4F4] to-[#F4F4F4] bg-[url('/themes/mny/topolines.png')] `,
+    wrapper1: 'w-full h-full flex-1 flex flex-col ', // first div inside Layout
+    wrapper2: 'w-full h-full flex-1 flex flex-row lg:pt-[118px] ', // inside page header, wraps sidebar
+    wrapper3: 'flex flex-1 w-full border-2 flex-col border shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[calc(100vh_-_102px)]' , // content wrapepr
     iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
     icon: 'text-slate-400 hover:text-blue-500'
+  },
+
+  sectionArray: {
+    container: 'w-full grid grid-cols-6 md:grid-cols-12 ',
+    layouts: {
+        centered: 'max-w-[1020px] mx-auto md:gap-[12px] px-0 lg:px-[56px]',
+        fullwidth: ''
+    },
+    sizes: {
+        "1/4": { className: 'col-span-6 md:col-span-3', iconSize: 25 },
+        "1/3": { className: 'col-span-6 md:col-span-4', iconSize: 33 },
+        "1/2": { className: 'col-span-6 md:col-span-6', iconSize: 50 },
+        "2/3": { className: 'col-span-6 md:col-span-8', iconSize: 66},
+        "1": { className: 'col-span-6 md:col-span-9', iconSize: 75 },
+        "2":   { className: 'col-span-6 md:col-span-12', iconSize: 100 },
+    }
   },
   pageControls: {
     controlItem: 'pl-6 py-0.5 text-md cursor-pointer hover:text-blue-500 text-slate-400 flex items-center',
@@ -82,8 +101,80 @@ const theme = {
     2: '',
     3: ''
   },
-  navLabel: 'px-6 pb-1 pt-6 uppercase text-xs text-blue-400',
-  bg: 'bg-slate-100',
+  lexical: {
+    editorShell: "font-['Proxima_Nova'] font-[400] text-[16px] text-[#37576B] leading-[22.4px]",
+    heading: {
+      h1: "font-[500]  text-[#2D3E4C] text-[36px] leading-[36px] tracking-[-.02em] font-[500] underline-offset-8 underline decoration-4 decoration-[#EAAD43] uppercase font-['Oswald'] pb-[12px]", //'PlaygroundEditorTheme__h1',
+      h2: "font-[500]  text-[#2D3E4C] text-[24px] leading-[24px] scroll-mt-36 font-['Oswald'] pb-[12x]", //'PlaygroundEditorTheme__h2',
+      h3: "font-[500]  text-[#2D3E4C] text-[16px] leading-[16px] scroll-mt-36 font-['Oswald'] pb-[12x]", //'PlaygroundEditorTheme__h3',
+      h4: "font-medium text-[#2D3E4C] scroll-mt-36 font-display", //'PlaygroundEditorTheme__h4',
+      h5: "scroll-mt-36 font-display", //'PlaygroundEditorTheme__h5',
+      h6: "scroll-mt-36 font-display", //'PlaygroundEditorTheme__h6',
+    },
+    paragraph: "m-0 relative", //'PlaygroundEditorTheme__paragraph',
+    quote:
+    "m-0 mb-2 font-['Oswald'] text-[30px] leading-[36px] text-[#2D3E4C] border-l-4 border-[#37576B] pl-4 pb-[12px]", //'PlaygroundEditorTheme__quote',
+
+  },
+  dataCard: {
+    columnControlWrapper: 'grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-x-1 gap-y-0.5',
+    columnControlHeaderWrapper: `px-1 font-semibold border bg-gray-50 text-gray-500`,
+
+    mainWrapperCompactView: 'grid',
+    mainWrapperSimpleView: 'flex flex-col',
+
+    subWrapper: 'w-full text-[#2D3E4C]',
+    subWrapperCompactView: 'flex flex-col flex-wrap rounded-[12px] divide-y',
+    subWrapperSimpleView: 'grid',
+
+    headerValueWrapper: 'w-full rounded-[12px] flex items-center gap-[8px] justify-center p-2',
+    headerValueWrapperCompactView: 'rounded-none py-[12px]',
+    headerValueWrapperSimpleView: '',
+    justifyTextLeft: 'text-start justify-items-start',
+    justifyTextRight: 'text-end justify-items-end',
+    justifyTextCenter: 'text-center justify-items-center',
+
+    textXS: 'font-medium font-[Oswald] text-[12px] leading-[140%]',
+    textXSReg: 'font-normal font-[Proxima Nova] text-[12px] leading-[100%] uppercase',
+    textSM: 'font-medium font-[Oswald] text-[14px] leading-[100%] uppercase',
+    textSMReg: 'font-normal font-[Proxima Nova] text-[14px] leading-[140%]',
+    textSMBold: 'font-normal font-[Proxima Nova] text-[14px] leading-[140%]',
+    textSMSemiBold: 'font-semibold font-[Proxima Nova] text-[14px] leading-[140%]',
+    textMD: 'font-medium font-[Oswald] text-[16px] leading-[100%] uppercase',
+    textMDReg: 'font-normal font-[Proxima Nova] text-[16px] leading-[140%]',
+    textMDBold: 'font-bold font-[Proxima Nova] text-[16px] leading-[140%]',
+    textMDSemiBold: 'font-semibold font-[Proxima Nova] text-[16px] leading-[140%]',
+    textXL: 'font-medium font-[Oswald] text-[20px] leading-[100%] uppercase',
+    textXLSemiBold: 'font-semibold font-[Proxima Nova] text-[20px] leading-[120%]',
+    text2XL: 'font-medium font-[Oswald] text-[24px] leading-[100%] uppercase',
+    text2XLReg: 'font-regular font-[Oswald] text-[24px] leading-[120%] uppercase',
+    text3XL: 'font-medium font-[Oswald] text-[30px] leading-[100%] uppercase tracking-[-0.05em]',
+    text3XLReg: 'font-normal font-[Oswald] text-[30px] leading-[120%] uppercase',
+    text4XL: 'font-medium font-[Oswald] text-[36px] leading-[100%] uppercase tracking-[-0.05em]',
+    text5XL: 'font-medium font-[Oswald] text-[48px] leading-[100%] uppercase tracking-[-0.05em]',
+    text6XL: 'font-medium font-[Oswald] text-[60px] leading-[100%] uppercase',
+    text7XL: 'font-medium font-[Oswald] text-[72px] leading-[100%] uppercase tracking-normal',
+    text8XL: 'font-medium font-[Oswald] text-[96px] leading-[95%] uppercase tracking-normal ',
+
+    imgXS: "max-w-16 max-h-16",
+    imgSM: "max-w-24 max-h-24",
+    imgMD: "max-w-32 max-h-32",
+    imgXL: "max-w-40 max-h-40",
+    img2XL: "max-w-48 max-h-48",
+    img3XL: "max-w-56 max-h-56",
+    img4XL: "max-w-64 max-h-64",
+    img5XL: "max-w-72 max-h-72",
+    img6XL: "max-w-80 max-h-80",
+    img7XL: "max-w-96 max-h-96",
+    img8XL: "max-w-128 max-h-128",
+
+    header: 'flex-1 uppercase text-[#37576B]',
+    headerCompactView: '',
+    headerSimpleView: '',
+    value: 'text-[#2D3E4C]',
+    valueCompactView: '',
+    valueSimpleView: ''
+  },
   sidenav: {
     fixed: ``,
     logoWrapper: `bg-neutral-100 text-slate-800`,
@@ -94,61 +185,133 @@ const theme = {
     
     itemsWrapper: `border-slate-200`,
     navItemContent: `transition-transform duration-300 ease-in-out flex-1`,
+    navItemContents: [
+      `text-[16px] font-['Oswald'] font-[500] leading-[16px]  text-[#2D3E4C] pl-4 py-3`, 
+      `text-[16px] font-['Proxima_Nova'] font-[600] leading-[19.2px] text-[#37576B] pl-4 py-3`,
+      `text-[14px] font-['Proxima_Nova'] font-[400] leading-[19.6px] text-[#37576B] pl-4 py-2`,
+      `text-[14px] font-['Proxima_Nova'] font-[400] leading-[19.6px] text-[#37576B] pl-4 py-2`,
+      `text-[14px] font-['Proxima_Nova'] font-[400] leading-[19.6px] text-[#37576B] pl-4 py-2`
+    ],
     navitemSide: `
         group  flex flex-col
-        pl-3 py-2 text-[14px] font-[Oswald] font-medium  text-slate-700 ml-2  border-white 
+        border-white 
         focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300
-        transition-all cursor-pointer
+        transition-all cursor-pointer 
      `,
     navitemSideActive: `
         group  flex flex-col
-        group flex pl-3 py-2 text-[14px] font-[Oswald] font-medium text-slate-700 ml-2   border-white 
         focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300
-        transition-all cursor-pointer
+        transition-all cursor-pointer border-l-2 border-slate-600 
 
       `,
-    indicatorIcon: 'fa fa-angle-right pt-2.5',
-    indicatorIconOpen: 'fal fa-angle-down pt-2.5',
+    indicatorIcon: "ArrowRight",
+    indicatorIconOpen: "ArrowDown",
     subMenuWrapper: `pl-2 w-full`,
+    subMenuOuterWrappers: ['pl-4'],
+    subMenuWrappers: ['w-full bg-[#F3F8F9] rounded-[12px] py-[12px]','w-full bg-[#E0EBF0]'],
     subMenuParentWrapper: `flex flex-col w-full`,
     subMenuWrapperTop: '',
   },
   topnav: {
-      topnavWrapper: `w-full bg-white h-20 flex items-center mt-6 mb-1 rounded-lg shadow `,
+      fixed: 'mt-8',
+      topnavWrapper: `px-[24px] py-[16px] w-full bg-white h-20 flex items-center rounded-lg shadow pointer-events-auto`,
       topnavContent: `flex items-center w-full h-full  max-w-[1400px] mx-auto `,
       topnavMenu: `hidden  md:flex items-center flex-1  h-full overflow-x-auto overflow-y-hidden scrollbar-sm`,
-      menuItemWrapper: 'flex',
+      mobileButton:`md:hidden bg-slate-100 inline-flex items-center justify-center pt-[12px] px-2 hover:text-blue-400  text-gray-400 hover:bg-gray-100 `,
+      indicatorIcon: 'fal fa-angle-down pl-2 pt-1',
+      indicatorIconOpen: 'fal fa-angle-down pl-2 pt-1',
+      menuItemWrapper1: [
+        ' ',
+        ''
+      ],
+      menuItemWrapper1Parent: [
+        ' ',
+        'bg-[#F3F8F9] p-4 rounded-lg'
+      ],
+      menuItemWrapper2: [
+        'flex text-[#37576B] ',
+        ' '
+      ],
       menuIconTop: `text-blue-400 mr-3 text-lg group-hover:text-blue-500`,
       menuIconTopActive : `text-blue-500 mr-3 text-lg group-hover:text-blue-500`,
       menuOpenIcon: `fa-light fa-bars fa-fw`,
       menuCloseIcon: `fa-light fa-xmark fa-fw"`,
       navitemTop: `
-          w-fit group font-display whitespace-nowrap
-          flex tracking-widest items-center font-[Oswald] font-medium text-slate-700 text-[12px] px-6 h-12
-          focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 uppercase
+          w-fit group  whitespace-nowrap
+          flex items-center 
+          text-[16px] font-['Proxima_Nova'] font-[500] 
+          px-2
+          focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300
           transition cursor-pointer
       `,
       navitemTopActive:
-        ` w-fit group font-display whitespace-nowrap
-          flex tracking-widest items-center font-[Oswald] font-medium text-slate-700 text-[12px] px-6 h-12 text-blue-500
-          focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 uppercase
+        ` w-fit group  whitespace-nowrap
+          flex  items-center 
+          text-[16px] font-['Proxima_Nova'] font-[500] 
+          px-2 text-blue
+          focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300
           transition cursor-pointer 
         `,
+      navItemDescription: ['hidden',`text-[16px] font-['Proxima_Nova'] font-[400] text-[#37576B] text-wrap`],
       //`px-4 text-sm font-medium tracking-widest uppercase inline-flex items-center  border-transparent  leading-5 text-white hover:bg-white hover:text-darkblue-500 border-gray-200 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out h-full`,
       topmenuRightNavContainer: "hidden md:flex h-full items-center",
       topnavMobileContainer: "bg-slate-50",
      
-      mobileButton:`md:hidden bg-slate-100 inline-flex items-center justify-center pt-[12px] px-2 hover:text-blue-400  text-gray-400 hover:bg-gray-100 `,
-      indicatorIcon: 'fal fa-angle-down pl-2 pt-1',
-      indicatorIconOpen: 'fal fa-angle-down pl-2 pt-1',
-      subMenuWrapper: `hidden`, 
-      subMenuParentWrapper: 'hidden',
-      subMenuWrapperChild: `divide-x overflow-x-auto max-w-[1400px] mx-auto`,
-      subMenuWrapperTop: 'hidden',
-      subMenuWrapperInactiveFlyout: `absolute left-0 right-0  mt-8 normal-case bg-white shadow-lg z-10 p-2`,
-      subMenuWrapperInactiveFlyoutBelow: ` absolute ml-40 normal-case bg-white shadow-lg z-10 p-2`,
-      subMenuWrapperInactiveFlyoutDirection: 'grid grid-cols-4'
       
+      subMenuWrapper1: [
+        'absolute left-0 right-0 normal-case mt-4 z-10 px-4 pt-[42px] px-[62px] cursor-default'
+      ],
+      subMenuWrapper2: `bg-white flex items-stretch rounded-lg p-4 shadow`,
+      subMenuParentContent: 'basis-1/3  text-wrap pr-[64px]',
+      subMenuParentName: `text-[36px] font-['Oswald'] font-500 text-[#2D3E4C] uppercase pb-2`,
+      subMenuParentDesc: `text-[16px] font-['Proxima_Nova'] font-[400] text-[#37576B]`,
+      subMenuParentLink: `w-fit h-fit cursor-pointer uppercase border boder-[#E0EBF0] bg-white hover:bg-[#E0EBF0] text-[#37576B] font-[700] leading-[14.62px] rounded-full text-[12px] text-center py-[16px] px-[24px]`,
+      subMenuItemsWrapperParent: 'grid grid-cols-2 gap-1 flex-1',
+      subMenuItemsWrapper: 'grid grid-cols-4 flex-1'
+
+      
+  },
+  table: {
+      tableContainer: 'relative flex flex-col w-full h-full overflow-x-auto scrollbar-sm border rounded-t-[12px]',
+      tableContainerNoPagination: 'rounded-b-[12px]',
+      tableContainer1: 'flex flex-col no-wrap min-h-[200px] max-h-[calc(78vh_-_10px)] overflow-y-auto scrollbar-sm',
+      headerContainer: 'sticky top-0 grid ',
+      thead: 'flex justify-between',
+      theadfrozen: '',
+      thContainer: 'w-full font-[500] py-4 pl-4 pr-0 font-[Oswald] text-[12px] uppercase text-[#2d3e4c] border-x',
+      thContainerBg: 'bg-[#F3F8F9] text-gray-900',
+      thContainerBgSelected: 'bg-gray-50 text-gray-900',
+      cell: 'relative flex items-center min-h-[36px]  border border-slate-50',
+      cellInner: `
+          w-full min-h-full flex flex-wrap items-center truncate py-1 px-2
+          font-['Proxima_Nova'] font-[400] text-[14px] text-[#37576B] leading-[20px]
+      `,
+      cellBg: 'bg-white',
+      cellBgSelected: 'bg-blue-50',
+      cellFrozenCol: '',
+      paginationContainer: 'w-full p-2 rounded-b-[12px] bg-[#F3F8F9] flex items-center justify-between',
+      paginationInfoContainer: '',
+      paginationPagesInfo: 'font-[500] font-[Oswald] text-[12px] uppercase text-[#2d3e4c] leading-[18px]',
+      paginationRowsInfo: 'text-xs font-[Proxima Nova] leading-[14px]',
+      paginationControlsContainer: 'flex flex-row items-center border rounded-[8px] overflow-hidden',
+      pageRangeItem: 'cursor-pointer px-[12px]  py-[7px] font-[Oswald] font-[500] text-[12px] border-r last:border-none uppercase leading-[18px]' ,
+      pageRangeItemInactive: 'bg-white text-[#2D3E4C]',
+      pageRangeItemActive: 'bg-[#2D3E4C] text-white',
+      openOutContainerWrapper: 'absolute inset-0 right-0 h-full w-full z-[100]',
+      openOutHeader: 'font-semibold font-[Proxima Nova] text-[#37576B] text-[14px] leading-[17.05px]',
+      openOutValue: 'font-normal font-[Proxima Nova] text-[#37576B] text-[14px] leading-[19.6px]',
+      openOutTitle: 'font-medium font-[Oswald] text-[24px] leading-[100%] uppercase text-[#2D3E4C]'
+  },
+  attribution: {
+    wrapper: 'w-full flex flex-col gap-[4px] text-[#2D3E4C] text-xs',
+    label: 'font-semibold text-[12px] leading-[14.62px] border-t pt-[14px]',
+    link: 'font-normal leading-[14.62px] text-[12px] underline'
+  },
+  label: {
+    labelWrapper: 'w-full px-[12px] pt-[9px] pb-[7px] bg-[#C5D7E0] hover:bg-[#E0EBF0] group rounded-[1000px]',
+    labelWrapperDisabled: 'px-[12px] pt-[9px] pb-[7px] bg-[#F3F8F9] group rounded-[1000px]',
+    label: 'text-[12px] text-[#37576B] font-bold leading-[14.62px]',
+    labelDisabled: 'text-[12px] text-[#C5D7E0] font-bold leading-[14.62px]',
   }
 }
 
@@ -216,20 +379,14 @@ export const themeOptions = {
         "type": "select",
         "options": [
           "none",
-          "micro",
-          "mini",
-          "compact",
           "full"
         ]
       },
-      "color": {
-        "label": "Color",
+      "depth": {
+        "label": "Depth",
         "type": "select",
         "options": [
-          "transparent",
-          "white",
-          "bright",
-          "dark"
+          1,2
         ]
       },
       "logo": {
